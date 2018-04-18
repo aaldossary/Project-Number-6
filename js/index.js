2f169a421314d1b8273ef9256e7e0906e93e3839
aaldossary <35977760+aaldossary@users.noreply.github.com>
2018-04-17 19:22:36 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  var modal = document.getElementById("loginModal");
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  $(".goToSite").click(function() {
+    $(".lightbox").fadeOut(2200);
+  });
+});
+
+var i = 0;
+var txt = "aaldossary, Abdullah Email: aaldossary@una.edu.";
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("showMyInfo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
